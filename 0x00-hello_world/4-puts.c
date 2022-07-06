598b3dcb0eaecbcd9f062f174d3a8bfb7bf64079
the-odyssey-21 <markizie@gmail.com>
2022-07-06 08:50:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *
+ *main - A program that outputs a line of text using puts()
+ *Return: 0 upon completion
+ */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
