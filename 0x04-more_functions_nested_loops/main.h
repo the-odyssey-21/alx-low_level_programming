0805de4095cd981c57b9c7b8654f7668ff59933e
the-odyssey-21 <markizie@gmail.com>
2022-07-07 15:16:00 -0700

    Mutiplies

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
 
-#endif
+#include <stdio.h>
+
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int mul(int a, int b);
+
+#endif /* MAIN_H */
