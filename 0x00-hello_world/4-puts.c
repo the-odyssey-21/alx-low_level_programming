df22f02c8a21b8720a22961cd891866aa01825b4
the-odyssey-21 <markizie@gmail.com>
2022-08-13 12:51:30 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
+
 /**
- *
- *main - A program that outputs a line of text using puts()
- *Return: 0 upon completion
+ * main - A simple program that outputs a line of text using puts()
+ * Return: 0 upon completion
  */
-int main (void)
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
